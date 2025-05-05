6083fa3d0cfad9b9efcb14e8d58d42630ec9574a
lukasz-kkk <lukaluzny17@gmail.com>
2025-05-05 16:28:07 +0200

    get-files

--- backend/Infrastructure/FileRepository.cs
+++ backend/Infrastructure/FileRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Domain;
 using Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure
 {
@@ -8,6 +9,16 @@ namespace Infrastructure
         public FileRepository(AppDbContext appDbContext)
             : base(appDbContext, appDbContext.Files)
         {
+        }
+
+        public new List<File> GetList(Func<File, bool> predicate = null)
+        {
+            if (predicate == null)
+                return _appDbContext.Files.ToList();
+            return _appDbContext.Files
+                .Include(file => file.Fundraiser)
+                .Where(predicate)
+                .ToList();
         }
     }
 }
