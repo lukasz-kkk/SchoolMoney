eebaaa5039f0a3db0d01c1eaec9969b98bb71dd6
lukasz-kkk <lukaluzny17@gmail.com>
2025-05-06 16:26:39 +0200

    Update user personal data

---
+++ backend/SchoolMoney/Commands/UpdateUserPersonalData.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace SchoolMoney.Commands
+{
+    public class UpdateUserPersonalData : IRequest<Unit>
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+    }
+}
