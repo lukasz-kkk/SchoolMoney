fe6c19eed955bc2f2173313b5355e8346afd614f
lukasz-kkk <lukaluzny17@gmail.com>
2025-05-08 10:37:59 +0200

    sender -> Sender in db

--- backend/Domain/Message.cs
+++ backend/Domain/Message.cs
@@ -2,7 +2,7 @@
 
 public class Message : Entity
 {
-    public User sender { get; set; }
+    public User Sender { get; set; }
     public int ReceiverClassId { get; set; }
     public int ReceiverUserId { get; set; }
     public string Content { get; set; }
