0c5abf937bdd4cfa93290fcbdb20bfb0ac61691c
lukasz-kkk <lukaluzny17@gmail.com>
2025-05-08 10:48:09 +0200

    class -> group in db

--- backend/Domain/Message.cs
+++ backend/Domain/Message.cs
@@ -3,7 +3,7 @@
 public class Message : Entity
 {
     public User Sender { get; set; }
-    public int ReceiverClassId { get; set; }
+    public int ReceiverGroupId { get; set; }
     public int ReceiverUserId { get; set; }
     public string Content { get; set; }
     public DateTime CreatedAt { get; set; }
